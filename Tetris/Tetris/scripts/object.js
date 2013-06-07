743587f2cfaa70adb1fbce59cad215325be0ee84
nader.a.dabour@gmail.com <nader.a.dabour@gmail.com@e797b18e-e6a7-e6a1-b59a-ed97964547da>
2013-06-07 10:38:47 +0000

    figureNS namespace

--- Tetris/Tetris/scripts/object.js
+++ Tetris/Tetris/scripts/object.js
@@ -1,65 +1,78 @@
-﻿// Figures
+﻿var figureNS = (function(){
 
-// Figure class
+    // Figures
 
-var Figure = $.inherit({
-    __constructor: function (form) {
-        this.form = form;
+    // Figure class
+
+    var Figure = $.inherit({
+        __constructor: function (form) {
+            this.form = form;
+        }
+    });
+
+    // I
+    var FigureI = $.inherit(Figure, {
+        __constructor: function () {
+            var form = [[1, 1, 1, 1]];
+            this.__base(form);
+        }
+    });
+
+    // J
+    var FigureJ = $.inherit(Figure, {
+        __constructor: function () {
+            var form = [[1, 0, 0], [1, 1, 1]];
+            this.__base(form);
+        }
+    });
+
+    // L
+    var FigureL = $.inherit(Figure, {
+        __constructor: function () {
+            var form = [[0, 0, 1], [1, 1, 1]];
+            this.__base(form);
+        }
+    });
+
+    // O
+    var FigureO = $.inherit(Figure, {
+        __constructor: function () {
+            var form = [[1, 1], [1, 1]];
+            this.__base(form);
+        }
+    });
+
+    // S
+    var FigureS = $.inherit(Figure, {
+        __constructor: function () {
+            var form = [[0, 1, 1], [1, 1, 0]];
+            this.__base(form);
+        }
+    });
+
+    // T
+    var FigureT = $.inherit(Figure, {
+        __constructor: function () {
+            var form = [[0, 1, 0], [1, 1, 1]];
+            this.__base(form);
+        }
+    });
+
+    // Z
+    var FigureZ = $.inherit(Figure, {
+        __constructor: function () {
+            var form = [[1, 1, 0], [0, 1, 1]];
+            this.__base(form);
+        }
+    });
+
+    return {
+        I: FigureI,
+        J: FigureJ,
+        L: FigureL,
+        T: FigureT,
+        O: FigureO,
+        S: FigureS,
+        Z: FigureZ,
     }
-});
-
-// I
-var FigureI = $.inherit(Figure, {
-    __constructor: function () {
-        var form = [[1, 1, 1, 1]];
-        this.__base(form);
-    }
-});
-
-// J
-var FigureJ = $.inherit(Figure, {
-    __constructor: function () {
-        var form = [[1, 0, 0], [1, 1, 1]];
-        this.__base(form);
-    }
-});
-
-// L
-var FigureJ = $.inherit(Figure, {
-    __constructor: function () {
-        var form = [[0, 0, 1], [1, 1, 1]];
-        this.__base(form);
-    }
-});
-
-// O
-var FigureJ = $.inherit(Figure, {
-    __constructor: function () {
-        var form = [[1, 1], [1, 1]];
-        this.__base(form);
-    }
-});
-
-// S
-var FigureJ = $.inherit(Figure, {
-    __constructor: function () {
-        var form = [[0, 1, 1], [1, 1, 0]];
-        this.__base(form);
-    }
-});
-
-// T
-var FigureJ = $.inherit(Figure, {
-    __constructor: function () {
-        var form = [[0, 1, 0], [1, 1, 1]];
-        this.__base(form);
-    }
-});
-
-// Z
-var FigureJ = $.inherit(Figure, {
-    __constructor: function () {
-        var form = [[1, 1, 0], [0, 1, 1]];
-        this.__base(form);
-    }
-});
+} ());
