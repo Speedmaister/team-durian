88f627fe2d0b7a983c04c4e39f37e0fe0e342e7e
radosholev@gmail.com <radosholev@gmail.com@e797b18e-e6a7-e6a1-b59a-ed97964547da>
2013-06-15 17:32:34 +0000

    Restored engine.js

--- Tetris/Tetris/scripts/engine.js
+++ Tetris/Tetris/scripts/engine.js
@@ -110,11 +110,37 @@ var Engine = (function () {
     }
 
     function canMoveLeft() {
-        return that.figPosition > 0;
+        var canMoveLeft = true;
+        if (this.figPosition > 0) {
+            for (var i = 0; i < this.currentFigure.form.length; i++) {
+                if (this.matrix[i + this.currentRowIndex][that.figPosition - 1] != 0 && this.currentFigure.form[i][0] != 0) {
+                    canMoveLeft = false;
+                }
+            }
+        }
+        else {
+            canMoveLeft = false;
+        }
+
+        return canMoveLeft;
     }
 
     function canMoveRight() {
-        return that.figPosition + that.currentFigure.form[0].length < MatrixCols;
+        var canMoveRight = true;
+        if (that.figPosition + that.currentFigure.form[0].length < MatrixCols) {
+            var lastIndex = this.currentFigure.form[0].length - 1;
+            for (var i = 0; i < this.currentFigure.form.length; i++) {
+                if (this.matrix[i + this.currentRowIndex][that.figPosition + lastIndex + 1] != 0
+                    && this.currentFigure.form[i][lastIndex] != 0) {
+                    canMoveRight = false;
+                }
+            }
+        }
+        else {
+            canMoveRight = false;
+        }
+
+        return canMoveRight;
     }
 
     function canRotate() {
@@ -202,11 +228,11 @@ var Engine = (function () {
                 this.figPosition = MatrixCols / 2;
                 if (this.currentRowIndex == 0) {
                     console.log("Game Over");
-
+                    renderMatrix(this.matrix, this.table);
                     TetrisStorage._checkCurrentScore(score);
                     gameOver = true;
 
-                    
+
                 } else {
                     checkFullForRows();
                     dropFigure();
